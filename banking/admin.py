4d4ea466e0e6ce73d01efd2cadea6c72abd34be2
sergio-garcia <sergio@ginx.com.br>
2015-01-09 16:50:29 -0200

    Added Bank.

--- banking/admin.py
+++ banking/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Account, Transaction
+from .models import Account, Bank, Transaction
 
 
 class TransactionInline(admin.TabularInline):
@@ -13,4 +13,9 @@ class AccountAdmin(admin.ModelAdmin):
     inlines = [TransactionInline]
 
 
+class BankAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(Bank, BankAdmin)
--- banking/models.py
+++ banking/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 
-class Account(models.Model):
-    bank_id = models.IntegerField()
-    account_id = models.IntegerField()
+class Bank(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Account(models.Model):
+    bank = models.ForeignKey(Bank)
+    number = models.IntegerField()
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Transaction(models.Model):
@@ -14,3 +25,6 @@ class Transaction(models.Model):
     date = models.DateField()
     amount = models.DecimalField(decimal_places=4, max_digits=20)
     memo = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "%s %s" % (str(self.date), self.memo)
